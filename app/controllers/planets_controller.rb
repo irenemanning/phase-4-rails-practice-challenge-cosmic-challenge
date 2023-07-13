f0cfa37ede2ca5c03ea9ff2a9a3491f779f72dae
Irene <irenemanningm@gmail.com>
2023-07-13 13:28:42 -0500

    done

--- app/controllers/planets_controller.rb
+++ app/controllers/planets_controller.rb
@@ -1,10 +1,38 @@
 class PlanetsController < ApplicationController
   before_action :set_planet, only: %i[ show update destroy ]
 
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
   # GET /planets
   def index
     planets = Planet.all
 
     render json: planets
+  end
+  def show 
+    planet  = Planet.find(params[:id])
+    render json: planet
+  end
+  def create
+    planet = Planet.create!(planet_params)
+    render json: planet, status: :created
+  end
+  def update
+    planet = Planet.find(params[:id])
+    planet.update(planet_params)
+    render json: planet
+  end
+  def destroy
+    planet = Planet.find(params[:id])
+    planet.destroy
+    head :no_content
+  end
+
+  private
+
+  def planet_params
+    params.permit(:name, :distance_from_earth, :nearest_star, :image)
+  end
+  def render_not_found_response
+    render json: { error: "Planet not found" }, status: :not_found
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :missions, only: :create
-  resources :planets, only: :index
-  resources :scientists
+  resources :planets, only: [:index, :show, :create, :destroy, :update]
+  # resources :planets, only: :index
+  resources :scientists, only: :index
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
